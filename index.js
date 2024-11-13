0f12aa2bacef6998cd5ba5be5eca944a9e0c4895
eymenefealtun <eymenefealtun@hotmail.com>
2024-11-14 00:16:42 +0300

    feat: new

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 window.onload = function () {
-   increaseView();
-};
-function increaseView() {
-   var xmlHttp = new XMLHttpRequest();
-   xmlHttp.open("GET", "https://camo.githubusercontent.com/723e010e0873b7f9aa45047fb94073594cc81fae6ceb1aa93aac49dd5d6723b8/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d7461726f74323132", true); // false for synchronous request
-   xmlHttp.send(null);
-}
-
+    increaseView();
+ };
+ function increaseView() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "https://camo.githubusercontent.com/723e010e0873b7f9aa45047fb94073594cc81fae6ceb1aa93aac49dd5d6723b8/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d7461726f74323132", true); // false for synchronous request
+    xmlHttp.send(null);
+ }
